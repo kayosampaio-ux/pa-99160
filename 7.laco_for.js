2c01c6287c3c78f402b6e737c9eb97babfdb62fd
gus <gustavo.o.silva7@ba.estudante.senai.br>
2026-02-26 21:28:26 -0300

    usdjnd

---
+++ 7.laco_for.js
@@ -0,0 +1,5 @@
+// LAÇO DE REPETIÇÃO - FOR
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
---
+++ 9.operadores.js
@@ -0,0 +1,26 @@
+let idade1 = 10
+let idade2 = "10"
+
+console.log(" == Comparando conteúdo: , ==");
+console.log(`O valor ${idade1} é igual a ${idade2})`);
+console.log(idade1 != idade2)
+
+console.log(" == Comparando conteúdo: , ==");
+console.log(`O valor ${idade1} é igual a ${idade2})`);
+console.log(idade1 != idade2)
+
+console.log(" == Comparando conteúdo: , ==");
+console.log(`O valor ${idade1} é igual a ${idade2})`);
+console.log(idade1 != idade2)
+
+console.log(" == Comparando conteúdo: , ==");
+console.log(`O valor ${idade1} é igual a ${idade2})`);
+console.log(idade1 != idade2)
+
+console.log(" == AND ==");
+console.log("1 < 2 && 1 < 3");
+console.log(1 < 2 && 1 < 3)
+
+console.log("\n== OR ==");
+console.log("10 < 2 || 1 < 3")
+console.log(10 < 2 || 1 < 3)
